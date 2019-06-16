1252e8f7ac0c3c3eec931023d5fcbfe70c9e3739
sieem <siem.linx@gmail.com>
2019-06-16 22:14:37 +0200

    redirect logout to login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -38,7 +38,7 @@ export class AuthService {
 
   logoutUser() {
     localStorage.removeItem('token')
-    this.router.navigate(['/'])
+    this.router.navigate(['/login'])
   }
 
   getUserDetails() {
