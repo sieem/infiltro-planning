559f3fa1e2955f9afc1c35b078d7bddb9dfdacfb
sieem <siem.linx@gmail.com>
2019-11-08 22:16:06 +0100

    remember scrollpos for projects page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd, NavigationStart, Scroll, ActivationStart, ResolveStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor () {
+  projectsPageActive: boolean = false
+  projectPageScrollPos: number = 0
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(async (ev) => {
+      // console.log(ev)
+      if (ev instanceof Scroll) {
+        if (ev.routerEvent.url == '/projecten') {
+          await this.delay(250)
+          window.scrollTo({ top: this.projectPageScrollPos })
+        }
+      }
+      if (ev instanceof NavigationEnd) {        
+        if (this.projectsPageActive) {
+          this.projectPageScrollPos = window.scrollY
+        }
+
+        this.projectsPageActive = (ev.url == '/projecten')
+       }
+    })
+  }
+
+  delay(t: number) {
+    return new Promise((resolve) => setTimeout(() => resolve(), t))
   }
 }
