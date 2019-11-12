923890712a8d3df3ec2c0e85921abafde27f0016
sieem <siem.linx@gmail.com>
2019-11-12 22:06:04 +0100

    allow uppercase in mails

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -6,7 +6,7 @@ import * as moment from 'moment';
 })
 export class FormService {
   // source: https://stackoverflow.com/questions/40513352/email-validation-using-form-builder-in-angular-2-latest-version
-  public emailRegex: string = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+  public emailRegex: string = "[A-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[A-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[A-z0-9](?:[A-z0-9-]*[A-z0-9])?\.)+[A-z0-9](?:[A-z0-9-]*[A-z0-9])?"
   // source: https://stackoverflow.com/a/19605207
   public passwordRegex: string = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$"
   public postalCodeRegex: string = "[0-9]{4}"
