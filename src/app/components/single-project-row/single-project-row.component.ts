4a6cc3f18872fb8f90823f0a706f27c366165e3c
Siem Lasseel <siem.linx@gmail.com>
2020-06-01 16:44:21 +0200

    Moving the last rows

--- src/app/components/single-project-row/single-project-row.component.ts
+++ src/app/components/single-project-row/single-project-row.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { SingleProjectService } from 'src/app/services/single-project.service';
 import { ProjectService } from 'src/app/services/project.service';
 import { FormService } from 'src/app/services/form.service';
+import { CompanyService } from 'src/app/services/company.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-single-project-row',
@@ -12,12 +14,15 @@ export class SingleProjectRowComponent implements OnInit {
   @Input('label') label: string;
   @Input('field') field: string;
   @Input('type') type: string;
+  @Input('valueKey') valueKey: string;
   @Input('dataSource') dataSource: [any];
 
 
   constructor(
     public singleProjectService: SingleProjectService,
     public projectService: ProjectService,
+    public companyService: CompanyService,
+    public userService: UserService,
     public formService: FormService,
   ) { }
 
