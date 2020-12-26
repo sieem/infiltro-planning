c6fe93fc5af20375a52118a0a3d8f111a91f9469
sieem <siem.linx@gmail.com>
2020-12-26 15:58:39 +0100

    Search function

--- src/app/pipes/filter-projects.pipe.ts
+++ src/app/pipes/filter-projects.pipe.ts
@@ -1,14 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { IProject } from '../interfaces/project.interface';
+import { UserPipe } from './user.pipe';
+import { CompanyPipe } from './company.pipe';
+import { ExecutorPipe } from './executor.pipe';
 
 @Pipe({
   name: 'filterProjects',
 })
 export class FilterProjectsPipe implements PipeTransform {
 
-  transform(projects: IProject[], activeFilter): IProject[] {
-    return projects.filter(row => {
-      let filterBooleans = []
+  constructor(
+    private userPipe: UserPipe,
+    private companyPipe: CompanyPipe,
+    private executorPipe: ExecutorPipe,
+  ) {}
+
+  async transform(projects: IProject[], activeFilter: any, searchTerm: string): Promise<IProject[]> {
+    return await this.asyncFilter(projects, async (row: IProject) => {
+      let filterBooleans = [];
+      let foundInSearch = false;
 
       for (let [key, values] of Object.entries(activeFilter)) {
         let filterArr: any = values
@@ -18,8 +28,39 @@ export class FilterProjectsPipe implements PipeTransform {
           filterBooleans.push(filterArr.includes(row[key]))
         }
       }
-      return !filterBooleans.includes(false)
+
+      if (searchTerm === "") {
+        // no search, so make it always found
+        foundInSearch = true;
+      } else {
+        for (let [key, value] of Object.entries(row)) {
+          if (['dateCreated', 'dateEdited', 'datePlanned', 'lat', 'lng', 'mails', 'comments', 'projectType', '_id', 'calendarId', 'calendarLink', 'eventId', 'hourPlanned', 'status', '__v'].includes(key)) {
+            continue;
+          }
+
+          switch (key) {
+            case 'EpbReporter': value = await this.userPipe.transform(value); break;
+            case 'company': value = await this.companyPipe.transform(value); break;
+            case 'executor': value = this.executorPipe.transform(value); break;
+          }
+
+          if (typeof value === 'string' && new RegExp(this.escapeRegExp(searchTerm), 'gi').test(value)) {
+            foundInSearch = true;
+            break;
+          }
+        }
+      }
+
+      return !filterBooleans.includes(false) && foundInSearch;
     });
   }
 
+  escapeRegExp(text: string): string {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  }
+
+  async asyncFilter (arr, predicate) {
+    // https://advancedweb.hu/how-to-use-async-functions-with-array-filter-in-javascript/
+    return arr.reduce(async (memo, e) => await predicate(e) ? [...await memo, e] : memo, []);
+  };
 }
