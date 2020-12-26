83addabcc407da473175735b870ebe59b32c7359
sieem <siem.linx@gmail.com>
2020-12-26 16:26:46 +0100

    Enable highlights and more fields to search on

---
+++ src/app/pipes/highlight-text.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { escapeRegExp } from '../helpers/escapeRegExp.helper';
+
+@Pipe({
+  name: 'highlightText'
+})
+export class HighlightTextPipe implements PipeTransform {
+
+  transform(value: string, searchTerm): string {
+    if (searchTerm === "" || !value) {
+      return value;
+    }
+    return value.replace(new RegExp(escapeRegExp(searchTerm), "gi"), match => `<mark>${match}</mark>`);
+  }
+
+}
