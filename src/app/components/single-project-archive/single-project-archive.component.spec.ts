994ba262773d2535592f7634705d51d501e5ab36
sieem <siem.linx@gmail.com>
2021-04-04 18:18:43 +0200

    Upgrade to angular 11

--- src/app/components/single-project-archive/single-project-archive.component.spec.ts
+++ src/app/components/single-project-archive/single-project-archive.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SingleProjectArchiveComponent } from './single-project-archive.component';
 
@@ -6,7 +6,7 @@ describe('SingleProjectArchiveComponent', () => {
   let component: SingleProjectArchiveComponent;
   let fixture: ComponentFixture<SingleProjectArchiveComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SingleProjectArchiveComponent ]
     })
--- src/app/components/single-project/single-project.component.spec.ts
+++ src/app/components/single-project/single-project.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SingleProjectComponent } from './single-project.component';
 
@@ -6,7 +6,7 @@ describe('SingleProjectComponent', () => {
   let component: SingleProjectComponent;
   let fixture: ComponentFixture<SingleProjectComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SingleProjectComponent ]
     })
